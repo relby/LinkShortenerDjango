db9f0dcfe8664955e7848fb51af09caeea7cc04f
VermanEZ <kudinov.nikita@gmail.com>
2021-09-18 20:10:48 +0300

    Setting up the main page(2)

--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(response):
     return render(response, "main/home.html", {})
+
+def test(response):
+    return render(response, "main/test.html", {})
